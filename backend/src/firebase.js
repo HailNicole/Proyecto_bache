07915b9c187ee0dabc3f88935a2d04fa38bad912
Nicol Constante <118192429+HailNicole@users.noreply.github.com>
2024-08-02 01:30:17 -0500

    Update firebase.js

--- backend/src/firebase.js
+++ backend/src/firebase.js
@@ -1,14 +1,9 @@
 const admin = require('firebase-admin');
-
 // Carga tu archivo de claves privadas JSON desde Firebase
 const serviceAccount = require('./secret.json');
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://gps-acc-default-rtdb.firebaseio.com/' // Cambia esto por tu URL de base de datos de Firebase
+  databaseURL: 'https://gps-acc-default-rtdb.firebaseio.com/'
 });
-
 const db = admin.database();
-
-
 module.exports = db;
